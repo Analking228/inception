879df330124f17215451976f2af75d38bb6ce72e
analking228 <kronos5525@yandex.ru>
2022-02-16 13:18:21 +0300

    changed wp.sh

--- srcs/containers/wordpress/wordpress.sh
+++ srcs/containers/wordpress/wordpress.sh
@@ -1,23 +1,53 @@
-sleep 30
+sleep 10
 mkdir -p /run/php/
 touch /run/php/php7.3-fpm.pid
 chown -R www-data:www-data /var/www/*
 chmod -R 755 /var/www/*
-echo "LOOOOOOOOOOOOOL"
 if [ -e /var/www/html/wp-config.php ]
 then
-	echo "Wordpress config file already exists"
+	echo "wp-config.php already exists"
 else
-  echo "LOOOOOOOOOOOOOOOOL"
    wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
    chmod +x wp-cli.phar
    mv wp-cli.phar /usr/local/bin/wp
    cd /var/www/html/
    wp core download --allow-root
-   mv ./wp-config.php /var/www/html
+   mv ./wp-config.php /var/www/html/
    echo "Configuring Wordpress parameters"
-   wp config create --allow-root --path=/var/www/html/ --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASS} --dbhost=${DB_HOST} --dbprefix=${DB_PREFIX}
-   wp core install --allow-root --path=/var/www/html/ --url=${DB_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_MAIL}
+   wp config create --allow-root --path=/var/www/html/ --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASS} --dbhost=${DB_HOST} --dbprefix=${DB_PREFIX} --skip-check
+   wp core install --allow-root --path=/var/www/html/ --url=${DB_URL} --title="My Title" --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_MAIL}
    wp user create --allow-root ${WP_NEW_USER} ${WP_NEW_USE_MAIL} --user_pass=${WP_NEW_USE_PASSWORD}
 fi                              
-exec "$@"
+ exec "$@"
+
+####################
+#   Options       #
+###################
+#
+#--url=<url>
+#--title=<site-title>
+#--admin_user=<username>
+#[--admin_password=<password>]
+#--admin_email=<email>
+#[--skip-email]
+
+
+#########################
+#   GLOBAL PARAMETERS   #
+#########################
+#
+#--path=<path>
+#--url=<url>
+#--ssh=[<scheme>:][<user>@]<host\|container>[:<port>][<path>]
+#--http=<http>
+#--user=<id\|login\|email>
+#--skip-plugins[=<plugins>]
+#--skip-themes[=<themes>]
+#--skip-packages
+#--require=<path>
+#--exec=<php-code>
+#--context=<context>
+#--[no-]color
+#--debug[=<group>]
+#--prompt[=<assoc>]
+#--quiet
