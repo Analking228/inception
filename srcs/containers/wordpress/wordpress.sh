ade68e6d6f4a9b05b8ebf7282c0975bff78f99d3
analking228 <kronos5525@yandex.ru>
2022-02-16 12:27:20 +0300

    changed wp.sh

--- srcs/containers/wordpress/wordpress.sh
+++ srcs/containers/wordpress/wordpress.sh
@@ -3,10 +3,8 @@ mkdir -p /run/php/
 touch /run/php/php7.3-fpm.pid
 chown -R www-data:www-data /var/www/*
 chmod -R 755 /var/www/*
-if [ -e /var/www/html/wp-config.php ]
+if ![ -e /var/www/html/wp-config.php ]
 then
-	touch /var/www/html/test.txt
-else
    wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
    chmod +x wp-cli.phar
    mv wp-cli.phar /usr/local/bin/wp
