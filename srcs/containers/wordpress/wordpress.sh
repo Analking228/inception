4e081e260d7dda52b0db888c8200cab0fb67d37c
analking228 <kronos5525@yandex.ru>
2022-02-16 13:39:20 +0300

    changed wp.sh

--- srcs/containers/wordpress/wordpress.sh
+++ srcs/containers/wordpress/wordpress.sh
@@ -9,6 +9,7 @@ else
    wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
    chmod +x wp-cli.phar
    mv wp-cli.phar /usr/local/bin/wp
+   vm ./wp-config.php /var/www/html/
    cd /var/www/html/
    wp core download --allow-root
    echo "Configuring Wordpress parameters"
