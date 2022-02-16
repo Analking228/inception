584aea4bbfe057bc7837dec25c94a24bd6b92c02
analking228 <kronos5525@yandex.ru>
2022-02-16 14:39:36 +0300

    wordpress.sh working fine (sleep)

--- srcs/containers/wordpress/wordpress.sh
+++ srcs/containers/wordpress/wordpress.sh
@@ -5,7 +5,7 @@ chown -R www-data:www-data /var/www/*
 chmod -R 755 /var/www/*
 if [ -e /var/www/html/wp-config.php ]
 then
-	touch /var/www/html/test.txt
+	echo "wp-config already exists"
 else
   wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
   chmod +x wp-cli.phar
@@ -15,7 +15,9 @@ else
   mv ./wp-config.php /var/www/html/
   echo "Configuring Wordpress parameters"
   wp config create --allow-root --path=/var/www/html/ --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASS} --dbhost=${DB_HOST} --dbprefix=${DB_PREFIX} --skip-check
+  echo "Installing Wordpress"
   wp core install --allow-root --path=/var/www/html/ --url=${DB_URL} --title="My Title" --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_MAIL}
+  echo "Creating additional user Alex"
   wp user create --allow-root ${WP_NEW_USER} ${WP_NEW_USE_MAIL} --user_pass=${WP_NEW_USE_PASSWORD}
 fi                              
 exec "$@"
