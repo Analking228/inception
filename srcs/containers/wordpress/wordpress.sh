b46bf011a4c74cf25f6598b8a6f28433ed0fe9fe
analking228 <kronos5525@yandex.ru>
2022-02-16 13:37:56 +0300

    changed wp.sh

--- srcs/containers/wordpress/wordpress.sh
+++ srcs/containers/wordpress/wordpress.sh
@@ -1,4 +1,3 @@
-sleep 10
 mkdir -p /run/php/
 touch /run/php/php7.3-fpm.pid
 chown -R www-data:www-data /var/www/*
@@ -12,10 +11,12 @@ else
    mv wp-cli.phar /usr/local/bin/wp
    cd /var/www/html/
    wp core download --allow-root
-   mv ./wp-config.php /var/www/html/
    echo "Configuring Wordpress parameters"
+   sleep 10
    wp config create --allow-root --path=/var/www/html/ --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASS} --dbhost=${DB_HOST} --dbprefix=${DB_PREFIX} --skip-check
+   sleep 10
    wp core install --allow-root --path=/var/www/html/ --url=${DB_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_MAIL}
+   sleep 10
    wp user create --allow-root ${WP_NEW_USER} ${WP_NEW_USE_MAIL} --user_pass=${WP_NEW_USE_PASSWORD}
 fi                              
  exec "$@"
